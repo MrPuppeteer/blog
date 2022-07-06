6eebc7c085cf02a0a49a42b9a711ae5257e3a632
MrPuppeteer <nuriksabagas@gmail.com>
2022-07-07 02:31:50 +0700

    feat: add projects data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,28 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Frontend Mentor Challenges',
+    description: 'This repo contains all challenges I have done on Frontend Mentor.',
+    imgSrc: '/static/images/frontend-mentor.png',
+    href: 'https://github.com/MrPuppeteer/frontend-mentor',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'The Odin Project',
+    description: 'This repo contains all projects I created on The Odin Project.',
+    imgSrc: '/static/images/theodinproject.png',
+    href: 'https://github.com/MrPuppeteer/the-odin-project',
+  },
+  {
+    title: 'Portfolio',
+    description:
+      'Portfolio website v1 created using React and Tailwind CSS. This web is based on React JS Portfolio by Code Commerce (@fireclint).',
+    imgSrc: '/static/images/portfolio.png',
+    href: 'http://mrpppteer.vercel.app/',
+  },
+  {
+    title: 'CS50 Homepage',
+    description: 'A simple homepage using HTML, CSS, and JavaScript.',
+    imgSrc: '/static/images/cs50.png',
+    href: 'https://mrpuppeteer.github.io/cs50/',
   },
 ]
 
