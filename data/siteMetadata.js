81909d4e09f370166e9587ed66e8f9861d3e0843
MrPuppeteer <nuriksabagas@gmail.com>
2022-07-07 00:42:32 +0700

    feat: personalize siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,22 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'MrPuppeteer',
+  author: 'MrPuppeteer',
+  headerTitle: 'MrPuppeteer',
+  description:
+    'Software Engineer — Random guy with great interest in web, data science, maths, and psych',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://mrpppteer-blog.vercel.app',
+  siteRepo: 'https://github.com/MrPuppeteer/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'nuriksabagas@gmail.com',
+  github: 'https://github.com/MrPuppeteer',
+  twitter: 'https://twitter.com/mrpppteer',
+  facebook: 'https://facebook.com/mrpppteer',
+  youtube: '',
+  linkedin: 'https://www.linkedin.com/in/bagasn',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
@@ -30,14 +31,14 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: '',
   },
   comment: {
     // If you want to use a commenting system other than giscus you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: '', // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
