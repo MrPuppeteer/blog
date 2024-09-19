b841127791ae1a8c33e5289b30ac1b01003f35c3
Bagas N <nuriksabagas@gmail.com>
2024-09-19 22:35:25 +0700

    docs: remove abandoned projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -14,29 +14,11 @@ const projectsData = [
     href: 'https://countdown.theatra.my.id/',
   },
   {
-    title: 'Frontend Mentor Challenges',
-    description: 'This repo contains all challenges I have done on Frontend Mentor.',
-    imgSrc: '/static/images/frontend-mentor.png',
-    href: 'https://github.com/MrPuppeteer/frontend-mentor',
-  },
-  {
-    title: 'The Odin Project',
-    description: 'This repo contains all projects I created on The Odin Project.',
-    imgSrc: '/static/images/theodinproject.png',
-    href: 'https://github.com/MrPuppeteer/the-odin-project',
-  },
-  {
     title: 'Portfolio',
     description:
       'Portfolio website v1 created using React and Tailwind CSS. This web is based on React JS Portfolio by Code Commerce (@fireclint).',
     imgSrc: '/static/images/portfolio.png',
     href: 'https://portfolio.theatra.my.id/',
-  },
-  {
-    title: 'CS50 Homepage',
-    description: 'A simple homepage using HTML, CSS, and JavaScript.',
-    imgSrc: '/static/images/cs50.png',
-    href: 'https://mrpuppeteer.github.io/cs50/',
   },
 ]
 
