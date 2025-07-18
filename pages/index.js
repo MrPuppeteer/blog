90b173a60aad6049d5dce6aa3c771fca813e029f
Bagas N <nuriksabagas@gmail.com>
2025-07-18 10:57:52 +0700

    fix: prettier errors

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ posts }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Hello, I'm <span className="text-primary-500">MrPuppeteer</span>
         </h1>
@@ -28,7 +28,7 @@ export default function Home({ posts }) {
         </p>
       </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-4 md:space-y-5">
+        <div className="space-y-2 pb-4 pt-6 md:space-y-5">
           <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
             Latest
           </h2>
