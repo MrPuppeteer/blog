fb70c03c1b012ac5cb5750d11029fb78f9d7da13
Bagas N <nuriksabagas@gmail.com>
2025-11-23 18:08:22 +0700

    feat: change project url to vercel.app

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,21 +4,21 @@ const projectsData = [
     description:
       'A simple tic-tac-toe game with instruction and history feature. Made with React and Tailwind CSS.',
     imgSrc: '/static/images/tictactoe.png',
-    href: 'https://tictactoe.theatra.my.id/',
+    href: 'https://tic-tac-toe-theatra.vercel.app/',
   },
   {
     title: 'Coundown App',
     description:
       'An app that shows how much time left until specified date & time. This app is made with Preact and Tailwind CSS.',
     imgSrc: '/static/images/countdown.png',
-    href: 'https://countdown.theatra.my.id/',
+    href: 'https://countdown-time.vercel.app/',
   },
   {
     title: 'Portfolio',
     description:
       'Portfolio website v1 created using React and Tailwind CSS. This web is based on React JS Portfolio by Code Commerce (@fireclint).',
     imgSrc: '/static/images/portfolio.png',
-    href: 'https://portfolio.theatra.my.id/',
+    href: 'https://mrpppteer.vercel.app/',
   },
 ]
 
